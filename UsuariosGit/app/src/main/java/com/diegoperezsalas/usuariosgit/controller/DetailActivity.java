8b529e8773e5d458979fef207d7b8146dc213685
Diego Perez Salas <diego.perez@elmundoplay.com>
2019-04-27 16:56:45 +0200

    Android Manifest Parent Activity

--- UsuariosGit/app/src/main/java/com/diegoperezsalas/usuariosgit/controller/DetailActivity.java
+++ UsuariosGit/app/src/main/java/com/diegoperezsalas/usuariosgit/controller/DetailActivity.java
@@ -1,5 +1,77 @@
 package com.diegoperezsalas.usuariosgit.controller;
 
-public class DetailActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.diegoperezsalas.usuariosgit.R;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView Link, Username;
+    Toolbar mActionBarToolbar;
+    ImageView imageView;
+
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageView = (ImageView) findViewById(R.id.user_image_header);
+        Username = (TextView) findViewById(R.id.username);
+
+        Link = (TextView) findViewById(R.id.link);
+
+        String username = getIntent().getExtras().getString("login");
+        String avatarURL = getIntent().getExtras().getString("avatar_url");
+        String link = getIntent().getExtras().getString("html_url");
+
+        Link.setText(link);
+        Linkify.addLinks(Link, Linkify.WEB_URLS);
+        Username.setText(username);
+
+        Glide.with(this).load(avatarURL).placeholder(R.drawable.load).into(imageView);
+
+    }
+
+   // getSupportActionBar().setTitle("Details Activity");
+
+    @Nullable
+    @Override
+    public ActionBar getSupportActionBar() {
+        return super.getSupportActionBar();
+    }
+
+   private Intent createShareForcastIntent(){
+       String username = getIntent().getExtras().getString("login");
+       String link = getIntent().getExtras().getString("link");
+       Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+       .setType("text/plain").setText("\n" + "Echa un vistazo a este increíble desarrollador @" + username + ", " + link)
+       .getIntent();
+       return shareIntent;
+   }
+
+
+    public boolean onCreateOptionMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForcastIntent());
+        return true;
+
+
+   }
 }
